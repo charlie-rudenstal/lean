fdb1f8a7722fc1979cf5b38ce78ae3084c31da96
charud <charud@spotify.com>
2017-05-21 13:46:23 +0200

    Add mutation

--- src/index.js
+++ src/index.js
@@ -2,7 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import ApolloClient, { createNetworkInterface } from 'apollo-client';
+import { ApolloProvider } from 'react-apollo';
 import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const client = new ApolloClient({
+  networkInterface: createNetworkInterface({
+    uri: 'https://api.graph.cool/simple/v1/cj2xhvkle2tf701101hcg646x'
+  })
+});
+
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+document.getElementById('root'));
 registerServiceWorker();
